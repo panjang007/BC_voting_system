b8f38f4f89f0af013884709f0f2f0ddd18bdae9d
Abu Zharr Luqman bin Abdul Aziz <32317099+panjang007@users.noreply.github.com>
2024-01-13 23:55:26 +0800

    Update function.dart

--- davote/lib/services/function.dart
+++ davote/lib/services/function.dart
@@ -1,71 +1,77 @@
-import 'dart:js_util';
-
 import 'package:davote/utils/constant.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-
 import 'package:web3dart/web3dart.dart';
 
-// Function to load the contract (you already have this)
+String? privateKey;
+int candidateID = 0;
+List<int> votesPerCandidate = [1, 2, 3];
+
 Future<DeployedContract> loadContract() async {
   String abi = await rootBundle.loadString('assets/abi.json');
   String contractAddress = myContractAddress;
 
-  final contract = DeployedContract(
-    ContractAbi.fromJson(abi, 'VotingSystem'),
-    EthereumAddress.fromHex(contractAddress),
-  );
+  final contract = DeployedContract(ContractAbi.fromJson(abi, 'VotingSystem'),
+      EthereumAddress.fromHex(contractAddress));
 
   return contract;
 }
 
-Future<String> callFunction(String funcname, List<dynamic> args,
+Future<String> callFunction(String functionName, List<dynamic> args,
     Web3Client ethClient, String privateKey) async {
   EthPrivateKey credentials = EthPrivateKey.fromHex(privateKey);
+  privateKey = credentials as String;
   DeployedContract contract = await loadContract();
-  final ethFunction = contract.function(funcname);
+  final ethFunction = contract.function(functionName);
   final result = await ethClient.sendTransaction(
       credentials,
       Transaction.callContract(
-        contract: contract,
-        function: ethFunction,
-        parameters: args,
-      ),
-      chainId: null,
-      fetchChainIdFromNetworkId: true);
+          contract: contract, function: ethFunction, parameters: args));
   return result;
 }
 
-Future<List<dynamic>> ask(
-    String funcName, List<dynamic> args, Web3Client ethClient) async {
-  final contract = await loadContract();
-  final ethFunction = contract.function(funcName);
-  final result =
-      ethClient.call(contract: contract, function: ethFunction, params: args);
-  return result;
+Future<String> checkValidVoter(String address, Web3Client ethClient) async {
+  var response = await callFunction(
+      'isValidVoter', [address], ethClient, privateKey as String);
+
+  return response;
 }
 
-// Function to check voter eligibility
-// Future<bool> isVoterEligible(
-//     DeployedContract contract, EthereumAddress voterAddress) async {
-//   final result = await contract.call<bool>('isValidVoter', [voterAddress]);
-//   return result;
-// }
-
-// Function to get candidate name
-Future<String> getCandidateName(
-    DeployedContract contract, int candidateID) async {
-  final result =
-      await callFunction('getCandidateName', [BigInt.from(candidateID)]);
-  return result;
+Future<String> getCandidateName(String address, Web3Client ethClient) async {
+  var response = await callFunction(
+      'isValidVoter', [address], ethClient, privateKey as String);
+  return response;
 }
 
-// Function to cast a vote
-Future<void> castVote(DeployedContract contract, int candidateID) async {
-  await callFunction('castVote', [BigInt.from(candidateID)]);
+Future<String> castVote(String address, Web3Client ethClient) async {
+  var response = await callFunction(
+      'isValidVoter', [address], ethClient, privateKey as String);
+  return response;
 }
 
-// Function to get the winner
-// Future<List<dynamic>> getWinner(DeployedContract contract) async {
-//   final result = await contract.call('getWinner');
-//   return result;
-// }
+Future<String> getWinner(String address, Web3Client ethClient) async {
+  int? winningVoteCount;
+  int? winningCandidateID;
+  var response = await callFunction('isValidVoter',
+      [winningVoteCount, winningCandidateID], ethClient, privateKey as String);
+
+  for (candidateID = 0; candidateID <= winningCandidateID!; candidateID++) {
+    if (votesPerCandidate[candidateID] > winningCandidateID) {
+      winningVoteCount = votesPerCandidate[candidateID];
+      winningCandidateID = candidateID;
+    }
+  }
+
+  return response;
+}
+
+
+
+
+
+
+
+
+// kena sambung part buat function untuk panggil function dalam smart contract
+
+// Dalam Video minit yang ke 40; 
